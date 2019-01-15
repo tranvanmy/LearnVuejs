206e8d2e8b811abdb3b85f7f58e30c1aaa16d931
NguyenCongViet <viet27.it@gmail.com>
2019-01-15 17:54:29 +0700

    2019_01_15: install template with vuejs

---
+++ NCV/resources/js/components/Admin.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <app-header></app-header>
+        <app-sidebar></app-sidebar>
+            <div class="content-wrapper"></div>
+        <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+import AppHeader from '@/components/header/Header';
+import AppSidebar from '@/components/sidebar/Sidebar';
+import AppFooter from '@/components/footer/Footer';
+
+    export default {
+        name: 'Admin',
+        components: {
+            AppHeader,
+            AppSidebar,
+            AppFooter,
+        }
+    }
+</script>
---
+++ NCV/resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Webapp</title>
+
+        <!-- Styles -->
+        <link rel="stylesheet" type="text/css" href="/css/app.css">
+
+        <!-- Google Font -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    </head>
+    <body class="hold-transition skin-blue sidebar-mini">
+        <div class="wrapper">
+            <div id="app"></div>
+        </div>
+        <script src="/js/app.js"></script>
+    </body>
+</html>
