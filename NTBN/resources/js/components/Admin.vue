6fddf866074c6f909a49c287c81beb2b454303aa
NTBNgoc <nguyenngoc.hust.97@gmail.com>
2019-01-15 18:24:04 +0700

    Learn Vuejs 15/01/1019

---
+++ NTBN/resources/js/components/Admin.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <app-header></app-header>
+        <app-aside></app-aside>
+        <app-footer></app-footer>
+        <app-control-siderbar></app-control-siderbar>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/header/Header';
+import Aside from '@/components/aside/Aside';
+import Footer from '@/components/footer/Footer';
+import ControlSiderbar from '@/components/control-siderbar/ControlSiderbar';
+
+export default {
+    name: 'Admin',
+    components: {
+        Header,
+        Aside,
+        AppFooter,
+        ControlSiderbar,
+    }
+}
